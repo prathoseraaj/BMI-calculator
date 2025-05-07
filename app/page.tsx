7ea79ba6a179c2e856c9a723559f55c1d4c818fa
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-07 15:02:35 +0530

    feat: input box for number to get height and weight

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,23 @@ import React from "react";
 const page = () => {
   return (
     <div className="flex justify-center items-center h-[100vh]">
-      <div className="flex flex-column h-[40vh] w-[600px] bg-white rounded-[10px] p-[10px]">
+      <div className="flex flex-col h-[40vh] w-[600px] bg-white rounded-[10px] p-[10px]">
         <div className="flex flex-col items-center w-full">
-          <h1 className="font-bold text-[25px]  ">
-            BMI Calculator
-          </h1>
-          <p className="text-gray-400 text-[20px]">Check your Body mass Index</p>
+          <h1 className="font-bold text-[25px]  ">BMI Calculator</h1>
+          <p className="text-gray-400 text-[20px]">
+            Check your Body mass Index
+          </p>
+        </div>
+        <div className="flex flex-row justify-center gap-30 mt-5">
+          <div >
+            <h1 className="font-bold text-[17px]  ">Height</h1>
+            <input type="number" name="height" className="w-[50px] border rounded mt-2" />
+          </div>
+          <div>
+            <h1 className="font-bold text-[17px] ">Weight</h1>
+            <input type="number" name="weight" className="w-[50px] border rounded mt-2" />
+
+          </div>
         </div>
       </div>
     </div>
