ae2746bde17841f53a54ab01be855039aa18eb70
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-07 14:52:45 +0530

    fix: light mode

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import React from "react";
 const page = () => {
   return (
     <div className="flex justify-center items-center h-[100vh]">
-      <div className="flex flex-column h-[40vh] w-[500px] bg-gray-900 rounded-[10px] p-[10px]">
-        <div className="flex justify-center w-full">
-          <h1 className="font-bold text-[20px] ">
+      <div className="flex flex-column h-[40vh] w-[600px] bg-white rounded-[10px] p-[10px]">
+        <div className="flex flex-col items-center w-full">
+          <h1 className="font-bold text-[25px]  ">
             BMI Calculator
           </h1>
-          <p></p>
+          <p className="text-gray-400 text-[20px]">Check your Body mass Index</p>
         </div>
       </div>
     </div>
