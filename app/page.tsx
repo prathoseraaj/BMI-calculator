c0bb396ef0ccd15fbd23c749989a3a9c0a6ddb16
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-07 14:35:11 +0530

    feat: headig

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div className='flex justify-center items-center h-[100vh]'>
-      <div>
-        
+    <div className="flex justify-center items-center h-[100vh]">
+      <div className="flex flex-column h-[40vh] w-[500px] bg-gray-900 rounded-[10px] p-[10px]">
+        <h1 className="font-bold text-[20px] flex justify-center w-full ">
+          BMI Calculator
+        </h1>
+        <div></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
