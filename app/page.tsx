1efd48499b467ec08339024006b0f890c86be670
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-07 14:36:10 +0530

    fix: tailwind accordingly

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ const page = () => {
   return (
     <div className="flex justify-center items-center h-[100vh]">
       <div className="flex flex-column h-[40vh] w-[500px] bg-gray-900 rounded-[10px] p-[10px]">
-        <h1 className="font-bold text-[20px] flex justify-center w-full ">
-          BMI Calculator
-        </h1>
-        <div></div>
+        <div className="flex justify-center w-full">
+          <h1 className="font-bold text-[20px] ">
+            BMI Calculator
+          </h1>
+          <p></p>
+        </div>
       </div>
     </div>
   );
